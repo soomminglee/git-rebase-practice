b74f44e514e8c7499d2e8cc1c4ca83fb3f38335b
soomminglee <soomming.lee@gmail.com>
2023-02-23 15:21:32 +0900

    conflict 해결😄

--- Frontend/git-rebase-test/src/Router.js
+++ Frontend/git-rebase-test/src/Router.js
@@ -7,8 +7,13 @@ export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
+<<<<<<< HEAD
         <Route path="/login" element={<Login />} />
         <Route path="/main" element={<Main />} />
+=======
+        <Route path="/" element={<Main />} />
+        <Route path="/signin" element={<Login />} />
+>>>>>>> 5837086 (Add: login 페이지 레이아웃 완료)
       </Routes>
     </BrowserRouter>
   );
